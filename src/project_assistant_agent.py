0d5c99e55a396cd4f9205a51f316feb2cc4a7639
Dbaibigh Abderrahmane <abderrahmanedbaibigh@gmail.com>
2025-08-15 15:06:53 +0200

    Updated

---
+++ src/project_assistant_agent.py
@@ -0,0 +1,60 @@
+from langgraph.graph import MessagesState
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from langgraph.prebuilt import create_react_agent, ToolNode
+from langchain_core.tools import tool
+
+
+class ProjectAnalysisAgentState(MessagesState):
+    """"State for the project analysis agent workflow."""
+
+project_assistant_agent_prompt = """
+    You are a coding assistant agent that helps users coding or analyze repositories and build projects.
+    If the supervisor request is unclear or incomplete, ask concise, targeted clarifying questions before taking action.
+    You will be given 2 types of requests from the supervisor:
+        1. A repository URL to download and analyze.
+        2. A simple help request to debug or optimize code.
+
+    1. When you receive a repository URL:
+        - Your tasks include:
+            1. Downloading repositories.
+            2. Analyzing repository content.
+            3. Assisting in project development.
+
+    2. When you receive a simple help request:
+        - Your tasks include:
+            1. Debugging code.
+            2. Optimizing code.
+            3. Providing clear, actionable steps based on the repository content.
+
+    General Guidelines:
+        - Always clarify user requests before proceeding with actions.
+        - Think step-by-step before acting.
+        - When a repository URL is given to you from the supervisor use the {download_zipped_repo} tool to download repositories.
+        - Analyze the downloaded repository content to provide insights and suggestions.
+        - When assisting in project development, focus on the repository's structure, files, and relevant code.
+        - If the user requests debugging or optimization, provide clear, actionable steps based on the repository content. 
+        - Maintain a friendly, helpful, and concise tone throughout the conversation.
+        - Your answer should be clear and structered, especially when presenting repository information or project steps.
+        - Use markdown formatting always for better readability, but ensure the content remains factual and relevant to the user's request.
+        
+"""
+
+
+@tool
+def download_zipped_repo(state):
+    """
+    Function to download the repository (.zip).
+    """
+    pass
+
+download_zipped_repo_tool = ToolNode(name="download_zipped_repo", func=download_zipped_repo)
+
+project_assistant_agent = create_react_agent(
+    name="project_assistant_agent",
+    description="A project assistant agent that can analyze repositories and assist in project building.",
+    llm="gpt-4",
+    tools=[download_zipped_repo_tool],
+    prompt=project_assistant_agent_prompt,
+    state_class=ProjectAnalysisAgentState
+).compile(name="project_assistant_agent")
